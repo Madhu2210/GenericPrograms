62bc2b12e55a9e0d312e84996065f5de1c7f34e1
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-07 22:01:51 +0530

    Added: Checking maximum String between 3.

--- src/com/bridglabz/GenericMax.java
+++ src/com/bridglabz/GenericMax.java
@@ -1,34 +1,47 @@
 package com.bridglabz;
 
 public class GenericMax {
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
         System.out.println("Welcome to Generic Program.");
-        Integer a = 2, b = 4, c = 6;
-        Float x = 2.3f, y = 7.8f, z = 6.7f;
-        getMaximum(a, b, c);
+        Integer a = 2, b = 4, c =6;
+        Float x = 2.3f, y =7.8f , z = 6.7f;
+        String l = "Apple" , m = "Banana" , n = "Orange";
+        getMaximum (a,b,c);
+        getMaximum (x,y,z);
+        getMaximum (l,m,n);
+    }
 
-        getMaximum(x, y, z);
+    private static void getMaximum(String a, String b, String c) {
+        String max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("Maximum Number of String is::"+max);
     }
 
     private static void getMaximum(Float a, Float b, Float c) {
         Float max = a;
-        if (b.compareTo(a) > 0) {
+        if(b.compareTo(a)>0){
             max = b;
         }
-        if (c.compareTo(max) > 0) {
+        if(c.compareTo(max)>0){
             max = c;
         }
-        System.out.println("Maximum Number of FloatValue is::" + max);
+        System.out.println("Maximum Number of FloatValue is::"+max);
     }
-    private static void getMaximum(Integer a, Integer b, Integer c) {
+
+    private static void  getMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
-        if (b.compareTo(a) > 0) {
+        if(b.compareTo(a)>0){
             max = b;
         }
-        if (c.compareTo(max) > 0) {
+        if(c.compareTo(max)>0){
             max = c;
         }
-        System.out.println("Maximum Number of Integer is::" + max);
+        System.out.println("Maximum Number of Integer is::"+max);
     }
 }
 
