020799aa6facd8de43d9260437681cdf60f15cbd
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-10 09:08:12 +0530

    Added: Finding maximum value of String.

--- src/com/bridglabz/GenericMax.java
+++ src/com/bridglabz/GenericMax.java
@@ -1,47 +1,47 @@
 package com.bridglabz;
 
 public class GenericMax {
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Welcome to Generic Program.");
-        Integer a = 2, b = 4, c =6;
-        Float x = 2.3f, y =7.8f , z = 6.7f;
-        String l = "Apple" , m = "Banana" , n = "Peach";
-        getMaximum (a,b,c);
-        getMaximum (x,y,z);
-        getMaximum (l,m,n);
+        Integer a = 2, b = 4, c = 6;
+        Float x = 2.3f, y = 7.8f, z = 6.7f;
+        String l = "Apple", m = "Banana", n = "Peach";
+        getMaximum(a, b, c);
+        getMaximum(x, y, z);
+        getMaximum(l, m, n);
     }
 
-    private static void getMaximum(String a, String b, String c) {
-        String max = a;
-        if(b.compareTo(a)>0){
-            max = b;
+    private static void getMaximum(String l, String m, String n) {
+        String max = l;
+        if (m.compareTo(l) > 0) {
+            max = m;
         }
-        if(c.compareTo(max)>0){
-            max = c;
+        if (n.compareTo(max) > 0) {
+            max = n;
         }
-        System.out.println("Maximum Number of String is::"+max);
+        System.out.println("Maximum Number of String is::" + max);
     }
 
     private static void getMaximum(Float a, Float b, Float c) {
         Float max = a;
-        if(b.compareTo(a)>0){
+        if (b.compareTo(a) > 0) {
             max = b;
         }
-        if(c.compareTo(max)>0){
+        if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println("Maximum Number of FloatValue is::"+max);
+        System.out.println("Maximum Number of FloatValue is::" + max);
     }
 
-    private static void  getMaximum(Integer a, Integer b, Integer c) {
+    private static void getMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
-        if(b.compareTo(a)>0){
+        if (b.compareTo(a) > 0) {
             max = b;
         }
-        if(c.compareTo(max)>0){
+        if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println("Maximum Number of Integer is::"+max);
+        System.out.println("Maximum Number of Integer is::" + max);
     }
 }
 
