03907f7df5f258649e43207f7b92657524a51e89
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-10 08:48:31 +0530

    Added: Finding the maximum integer.

--- src/com/bridglabz/GenericMax.java
+++ src/com/bridglabz/GenericMax.java
@@ -3,17 +3,17 @@ package com.bridglabz;
 public class GenericMax {
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Program.");
-        Integer a = 2, b = 4, c =6;
-        testMaximum (a,b,c);
+        Integer obj1 = 2, obj2 = 4, obj3 =6;
+        testMaximum (obj1, obj2, obj3);
     }
 
-    private static void testMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if(b.compareTo(a)>0){
-            max = b;
+    private static void testMaximum(Integer obj1, Integer obj2, Integer obj3) {
+        Integer max = obj1;
+        if(obj2.compareTo(obj1)>0){
+            max = obj2;
         }
-        if(c.compareTo(max)>0){
-            max = c;
+        if(obj3.compareTo(max)>0){
+            max = obj3;
         }
         System.out.println("Maximum Number is::"+max);
     }
