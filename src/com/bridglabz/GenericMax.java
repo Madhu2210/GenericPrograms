bf782c732ad7103d938b3bdb7c979883c2bb0e31
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-10 08:57:00 +0530

    Adding: Checking maximum Float Value.

--- src/com/bridglabz/GenericMax.java
+++ src/com/bridglabz/GenericMax.java
@@ -3,32 +3,32 @@ package com.bridglabz;
 public class GenericMax {
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Program.");
-        Integer a = 2, b = 4, c = 6;
+        Integer obj1 = 2, obj2 = 4, obj3 =6;
         Float x = 2.3f, y = 7.8f, z = 6.7f;
-        getMaximum(a, b, c);
+        getMaximum(obj1, obj2, obj3);
 
         getMaximum(x, y, z);
     }
 
-    private static void getMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(a) > 0) {
-            max = b;
+    private static void getMaximum(Float x, Float y, Float z) {
+        Float max = x;
+        if (y.compareTo(x) > 0) {
+            max = y;
         }
-        if (c.compareTo(max) > 0) {
-            max = c;
+        if (z.compareTo(max) > 0) {
+            max = z;
         }
-        System.out.println("Maximum Number of FloatValue is::" + max);
+        System.out.println("Maximum Number of Float Value is::" + max);
     }
-    private static void getMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(a) > 0) {
-            max = b;
+    private static void getMaximum(Integer obj1, Integer obj2, Integer obj3) {
+        Integer max = obj1;
+        if(obj2.compareTo(obj1)>0){
+            max = obj2;
         }
-        if (c.compareTo(max) > 0) {
-            max = c;
+        if(obj3.compareTo(max)>0){
+            max = obj3;
         }
-        System.out.println("Maximum Number of Integer is::" + max);
+        System.out.println("Maximum Number of Integer is::"+max);
     }
 }
 
